e812836958a5d6332dbcd671411957f9b9807983
Ian Pedraza <ianpedrazacelon@gmail.com>
2022-09-13 12:33:41 -0500

    first commit

---
+++ app/src/main/java/com/ianpedraza/clipingexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ianpedraza.clipingexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippedView(this))
+    }
+}
